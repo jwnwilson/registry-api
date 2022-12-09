83dd2bb376328f3b0dc58db6ce29eb11c4317693
Noel Wilson <jwnwilson88@gmail.com>
2022-12-09 00:01:19 +0000

    Removing extra param

--- src/app/use_case/entity.py
+++ src/app/use_case/entity.py
@@ -45,7 +45,5 @@ def create(
 def create_entities_from_file(
     entity_type: str, file: FileDTO, user: UserData, db_adapter: DbAdapter
 ) -> List[EntityDTO]:
-    entities_dto: List[EntityDTO] = entity.parse_json(
-        entity_type, file, user, db_adapter
-    )
+    entities_dto: List[EntityDTO] = entity.parse_json(entity_type, file, db_adapter)
     return entity.create_entities(entities_dto, db_adapter=db_adapter)
